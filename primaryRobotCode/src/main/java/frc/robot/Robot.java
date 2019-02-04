c4400879801839b434a3eb9a2d238630cf237a61
CMS Programming Laptop 1 <>
2019-02-04 16:25:13 -0600

    Updates

--- primaryRobotCode/src/main/java/frc/robot/Robot.java
+++ primaryRobotCode/src/main/java/frc/robot/Robot.java
@@ -9,14 +9,18 @@ package frc.robot;
 
 import edu.wpi.cscore.CvSink;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.NetworkButton;
 import edu.wpi.first.wpilibj.smartdashboard.*;
 import edu.wpi.first.wpilibj.shuffleboard.*;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Compressor;
@@ -31,22 +35,30 @@ import edu.wpi.first.wpilibj.DigitalInput;
  * project.
  */
 public class Robot extends TimedRobot {
+  private final NetworkTableInstance netInst = NetworkTableInstance.getDefault();
   private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  private final Spark leftMotor = new Spark(1);
-  private final Spark rightMotor = new Spark(0);
+  private final Spark arms = new Spark(0);
+  private final Spark rightBack = new Spark(1);
+  private final Spark rightFront = new Spark(2);
+  private final Spark leftFront = new Spark(3);
+  private final Spark leftBack = new Spark(4);
+  private final Spark elevator = new Spark(5);
+  private final SpeedControllerGroup leftMotors = new SpeedControllerGroup(leftBack, leftFront);
+  private final SpeedControllerGroup rightMotors = new SpeedControllerGroup(rightBack, rightFront);
   private final DifferentialDrive m_robotDrive
-      = new DifferentialDrive(rightMotor, leftMotor);
+      = new DifferentialDrive(rightMotors, leftMotors);
   private final Joystick m_stick = new Joystick(0);
   private final Timer m_timer = new Timer();
   private final Compressor mainCompressor = new Compressor(0);
-  private final DoubleSolenoid test1 = new DoubleSolenoid(0, 1);
-  private final JoystickButton solIn = new JoystickButton(m_stick, 5);
-  private final JoystickButton solOut = new JoystickButton(m_stick, 6);
+  private final DoubleSolenoid clawEject = new DoubleSolenoid(0, 1);
+  private final DoubleSolenoid discGrabber = new DoubleSolenoid(2, 3);
+  private final NetworkButton pcmResetButton = new NetworkButton(, "PCM Sticky Fault Reset");
+  //private final JoystickButton solIn = new JoystickButton(m_stick, 5);
+  //private final JoystickButton solOut = new JoystickButton(m_stick, 6);
   private final AnalogInput pressureSensor = new AnalogInput(0);
-  private final DigitalInput testSwitch = new DigitalInput(0);
+  //private final DigitalInput testSwitch = new DigitalInput(0);
 
   /**
    * This function is run when the robot is first started up and should be
@@ -55,11 +67,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    //m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-    //CameraServer.getInstance().startAutomaticCapture("Front Camera", "/dev/video0"); //Disabled, use raspberry pi instead
-    //CameraServer.getInstance().startAutomaticCapture("Rear Camera", "/dev/video1"); //Disabled, use raspberry pi instead
-    
   }
 
   /**
@@ -73,7 +81,14 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     SmartDashboard.putNumber("Pressure Sensor", 250 * (pressureSensor.getVoltage() / 5) - 20);
-    SmartDashboard.putBoolean("Switch", testSwitch.get());
+    SmartDashboard.putBoolean("PCM Fault", mainCompressor.getCompressorCurrentTooHighFault() || mainCompressor.getCompressorNotConnectedFault() || mainCompressor.getCompressorShortedFault());
+    SmartDashboard.putBoolean("PCM Sticky Fault", mainCompressor.getCompressorCurrentTooHighStickyFault() || mainCompressor.getCompressorNotConnectedStickyFault() || mainCompressor.getCompressorShortedStickyFault());
+    SmartDashboard.putBoolean("PCM Compressor Over Current", mainCompressor.getCompressorCurrentTooHighFault());
+    SmartDashboard.putBoolean("PCM Compressor Not Connected", mainCompressor.getCompressorNotConnectedFault());
+    SmartDashboard.putBoolean("PCM Compressor Shorted", mainCompressor.getCompressorShortedFault());
+    SmartDashboard.putBoolean("PCM Compressor Over Current [Sticky]", mainCompressor.getCompressorCurrentTooHighStickyFault());
+    SmartDashboard.putBoolean("PCM Compressor Not Connected [Sticky]", mainCompressor.getCompressorNotConnectedStickyFault());
+    SmartDashboard.putBoolean("PCM Compressor Shorted [Sticky]", mainCompressor.getCompressorShortedStickyFault());
   }
 
   /**
@@ -102,12 +117,9 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
       case kDefaultAuto:
       default:
-      m_robotDrive.arcadeDrive(0, 0.50);
+        // Put default auto code here
         break;
     }
   }
@@ -118,11 +130,6 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     m_robotDrive.arcadeDrive(-m_stick.getY(), m_stick.getZ());
-
-    //System.out.println(m_robotDrive.getDescription());
-
-    if (solIn.get()) test1.set(Value.kReverse);
-    if (solOut.get()) test1.set(Value.kForward);
   }
 
   /**
